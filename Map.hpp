d9ff277b967b4812bf17a7cc1d7329035b965f16
NeoBryant <1512917808@qq.com>
2017-09-08 00:46:09 +0800

    commit source codes

---
+++ Map.hpp
@@ -0,0 +1,24 @@
+//
+//	Map.hpp
+//	MineGame
+//	
+//	Created by Bryant on 2017/9/7.
+//	Copyright @ 2017 Bryant. All rights reserved.
+//
+
+#ifndef SYMBOL
+#define SYMBOL value
+
+class Map
+{
+public:
+	Map();
+	~Map();
+private:	
+	int **mineMap; 
+};
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//	main.cpp
+//	MineGame
+//	
+//	Created by Bryant on 2017/9/7.
+//	Copyright @ 2017 Bryant. All rights reserved.
+//
+
+#include <iostream>
+#include "Map.hpp"
+using namespace std;
+
+int main() {
+
+
+
+
+	return 0;
+}
